0337ea54da9164cb8520aeb69efe34ef85c8ffb2
umarekocakurt <umarekocakurt@localhost>
2020-02-20 20:56:50 -0500

    scanner class

--- Project1/src/a_module1/_1Scanner/LearnScanner.java
+++ Project1/src/a_module1/_1Scanner/LearnScanner.java
@@ -3,25 +3,30 @@ package a_module1._1Scanner;
 import java.util.Scanner;
 
 public class LearnScanner {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Welcome!");
-        
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome!");
+
 //        Should we have a space after ?
-        System.out.print("What is your name?");
-        String name = input.next();
-        System.out.println("Hello " + name);
-        
-        int count = input.nextInt();
-        double  amount = input.nextDouble();
+		System.out.print("What is your name? ");
+		//String name = input.next();
+		String name = input.nextLine();
+		System.out.println("Hello " + name);
 
-        String name = input.next();
-        int age = input.nextInt();
-        double weight = input.nextDouble();
+		System.out.print("Enter an integer ");
+		int count = input.nextInt();
+		System.out.println("you entered " + count);
+
+		System.out.print("Enter a double ");
+		double amount = input.nextDouble();
+		System.out.println("you entered " + amount);
+
+		name = input.next(); // Mesele? String name(can not redefine)
+		int age = input.nextInt();
+		double weight = input.nextDouble();
 //        What is wrong?
-        System.out.println(name + " is " + age + " year old and weighs " + weight + "kg”);
+		System.out.println(name + " is " + age + " year old and weighs " + weight + "lb");
 
-        
-        input.close();
-    }
+		input.close();
+	}
 }
